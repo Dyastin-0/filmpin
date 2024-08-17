df86a9fe9b33a5e411e794e35fcd0752bc64d3f4
Justine Paralejas <paralejasjustine15@gmail.com>
2024-08-18 01:21:46 +0800

    fixed button foreground @default

--- client/src/components/ui/Button.jsx
+++ client/src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const variants = {
-	default: 'p-2 rounded-md transition-all duration-300 focus:bg-primary-highlight focus:text-primary-highlight-foreground focus:text-secondary-foreground hover:bg-primary-highlight hover:text-primary-highlight-foreground',
+	default: 'p-2 rounded-md transition-all duration-300 focus:bg-primary-highlight focus:text-primary-highlight-foreground hover:bg-primary-highlight hover:text-primary-highlight-foreground',
 	link: 'text-primary-foreground font-semibold transition-all duration-300 hover:cursor-pointer hover:text-primary-highlight focus:shadow-[var(--highlight)_0_2px_0_0] pb-1',
 	ghost: 'text-primary-foreground bg-accent rounded-md shadow-sm p-2 focus:shadow-[var(--highlight)_0_2px_0_0] transition-all duration-300'
 }
