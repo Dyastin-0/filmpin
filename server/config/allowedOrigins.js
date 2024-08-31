ca55f627c3dbc24c54f4ee2f92ed1d2a5f2408f0
Justine Paralejas <paralejasjustine15@gmail.com>
2024-08-31 23:14:13 +0800

    updated cors options

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
 	'http://localhost:5173',
-	'https://filmpin.onrender.com'
+	'https://filmpin.onrender.com',
+	'https://filmpin.duckdns.org'
 ];
 
 module.exports = allowedOrigins;
