ed2bbf0781a2011da2fc69b0416590a2e5cb9f27
Justine Paralejas <paralejasjustine15@gmail.com>
2024-09-01 23:35:30 +0800

    small fix

--- client/src/components/Clip.jsx
+++ client/src/components/Clip.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Clip = ({ title, trailerKey, onClick }) => {
 	console.log(title)
 	return (
-		<div className='h-fit hover:cursor-pointer'
+		<div className='h-fit max-w-[270px] hover:cursor-pointer'
 			onClick={onClick}
 		>
 			<div className='relative'>
@@ -18,7 +18,7 @@ const Clip = ({ title, trailerKey, onClick }) => {
 					<FontAwesomeIcon icon={faPlay} className='text-accent text-2xl' />
 				</div>
 			</div>
-			<h1 className='text-center text-primary-foreground mt-2 text-xs line-clamp-1 text-ellipsis font-semibold'>{title}</h1>
+			<h1 className='text-center text-primary-foreground mt-2 text-xs line-clamp-1 font-semibold'>{title}</h1>
 		</div>
 	)
 }
