46c5c04e902baae7aa47dd657c55d7645bac6f5c
Justine Paralejas <paralejasjustine15@gmail.com>
2024-09-01 20:47:37 +0800

    added githubSignatureVerification

--- githook/index.js
+++ githook/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const { exec } = require('child_process');
+const crypto = require('crypto');
 
 const app = express();
 app.use(express.json());
 
-app.get('/webhook', (req, res) => {
+const GITHUB_SECRET = 'yanyang';
+
+function verifyGitHubSignature(req, res, next) {
+    const signature = req.headers['x-hub-signature-256'];
+    if (!signature) {
+        return res.status(401).send('No signature provided');
+    }
+
+    const hmac = crypto.createHmac('sha256', GITHUB_SECRET);
+    const digest = `sha256=${hmac.update(JSON.stringify(req.body)).digest('hex')}`;
+
+    if (crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest))) {
+        return next();
+    } else {
+        return res.status(401).send('Invalid signature');
+    }
+}
+
+app.post('/webhook', verifyGitHubSignature, (req, res) => {
     exec('cd ../client && git pull && npm i && npm run build && cd ../server && npm i && sudo systemctl restart filmpin.service && systemctl restart filmpinclient.service && systemctl restart caddy', (err, stdout, stderr) => {
         if (err) {
             console.error(`Error pulling repo: ${stderr}`);
