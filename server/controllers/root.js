d2b954d30d989264d21f737d5827b06ff929d9fe
Justine Paralejas <paralejasjustine15@gmail.com>
2024-09-01 20:40:34 +0800

    test ci/cd

--- server/controllers/root.js
+++ server/controllers/root.js
@@ -1,5 +1,5 @@
 const handleRoot = (_, res) => {
-	res.status(200).send('I love Rhea Jane Gamposilao.');
+	res.status(200).send('I love you so much Rhea Jane Gamposilao.');
 }
 
 module.exports = handleRoot;
