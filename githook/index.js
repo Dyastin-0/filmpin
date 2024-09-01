efb28d3d10149386abc4759cc263dd418d12018f
Justine Paralejas <paralejasjustine15@gmail.com>
2024-09-01 22:19:31 +0800

    test fix

--- githook/index.js
+++ githook/index.js
@@ -4,35 +4,39 @@ const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 
-// Middleware to parse the raw body for signature verification
-app.use(bodyParser.raw({ type: 'application/json' }));
+app.use(bodyParser.json({
+    verify: (req, _, buf, encoding) => {
+        if (buf && buf.length) {
+            req.rawBody = buf.toString(encoding || 'utf8');
+        }
+    },
+}));
 
+const sigHeaderName = 'x-hub-signature-256';
+const sigHashAlg = 'sha256';
 const GITHUB_SECRET = process.env.GITHUB_SECRET;
 
 const verifyGitHubSignature = (req, res, next) => {
-    const signature = req.headers['x-hub-signature-256']; // GitHub uses this header for the SHA-256 signature
-    if (!signature) {
-        return res.status(401).send('No signature provided');
-    }
+    if (!req.rawBody) return res.status(400).send('No body found');
 
-    // Create HMAC and compute digest using the raw body Buffer
-    const hmac = crypto.createHmac('sha256', GITHUB_SECRET);
-    const digest = `sha256=${hmac.update(req.body).digest('hex')}`; // Use raw body directly
+    const signature = req.get(sigHeaderName);
+    if (!signature) return res.status(400).send('No signature provided');
 
-    // Compare the computed digest with the signature from GitHub
+    const hmac = crypto.createHmac(sigHashAlg, GITHUB_SECRET);
+    const digest = `sha256=${hmac.update(req.rawBody).digest('hex')}`;
+
     if (crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest))) {
         return next();
     } else {
         return res.status(401).send('Invalid signature');
     }
-}
+};
 
-app.post('/webhook', verifyGitHubSignature, (req, res) => {
+app.post('/webhook', verifyGitHubSignature, (_, res) => {
     exec('cd ../client && git pull && npm i && npm run build && cd ../server && npm i && sudo systemctl restart filmpin.service && systemctl restart filmpinclient.service && systemctl restart caddy', (err, stdout, stderr) => {
         if (err) {
             console.error(`Error pulling repo: ${stderr}`);
