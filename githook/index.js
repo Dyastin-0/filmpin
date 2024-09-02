9013694f70695fe7113fa947ce32fe6a1a382e7b
Justine Paralejas <paralejasjustine15@gmail.com>
2024-09-02 13:01:36 +0800

    test ci/cd

--- githook/index.js
+++ githook/index.js
@@ -1,64 +1,89 @@
 const express = require('express');
+const { exec } = require('child_process');
+const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const { verifyGitHubSignature } = require('./middlewares/githubSignature');
-const { runCommandsInShell } = require('./helpers/childExec');
 
 dotenv.config();
+
 const app = express();
+const GITHUB_SECRET = process.env.GITHUB_SECRET;
+
 app.use(bodyParser.raw({ type: 'application/json' }));
 
-const hasChangesInDirectory = (commits, directory) => {
-  return commits.some(commit =>
-    commit.modified.concat(commit.added, commit.removed).some(file => file.startsWith(directory))
-  );
+const createHmacSignature = (req) => {
+  return crypto
+    .createHmac('sha256', GITHUB_SECRET)
+    .update(req.body)
+    .digest('hex');
+}
+
+function compareSignatures(signature, comparisonSignature) {
+  if (signature.length !== comparisonSignature.length) {
+    console.error('Signature lengths do not match:', signature.length, comparisonSignature.length);
+    return false;
+  }
+
+  const source = Buffer.from(signature, 'hex');
+  const comparison = Buffer.from(comparisonSignature, 'hex');
+  return crypto.timingSafeEqual(source, comparison);
+}
+
+const verifyGitHubSignature = (req, res, next) => {
+  const signature = req.headers['x-hub-signature-256'];
+  if (!signature) return res.status(400).send('Missing signature');
+
+  const hmac = 'sha256=' + createHmacSignature(req);
+
+  console.log('Received signature:', signature);
+  console.log('Calculated HMAC:', hmac);
+
+  const matched = compareSignatures(signature, hmac);
+
+  if (!matched) return res.status(400).send('Signature mismatched.');
+
+  next();
 };
 
-const updateAndRestartServices = (commits) => {
-  const commands = ['cd .. git pull'];
+const runCommandsInShell = (commands) => {
+  const command = commands.join(' && ');
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error executing commands: ${stderr}`);
+      reject(stderr);
+    } else {
+      console.log(`Commands output: ${stdout}`);
+      resolve(stdout);
+    }
+  });
+};
 
-  if (hasChangesInDirectory(commits, 'client/')) {
-    console.log('client/')
-    commands.push('cd ../client && npm install && npm run build');
-    commands.push('sudo systemctl restart filmpinclient.service');
-  }
-
-  if (hasChangesInDirectory(commits, 'server/')) {
-    console.log('server/')
-    commands.push('cd ../server && npm install');
-    commands.push('sudo systemctl restart filmpin.service');
-  }
-
-  if (commands.length > 0) {
-    commands.push('sudo systemctl restart caddy');
+const updateAndRestartServices = () => {
+  try {
+    const commands = [
+      'cd ../client && git pull && npm install && npm run build',
+      'cd ../server && npm install',
+      'sudo systemctl daemon-reload',
+      'sudo systemctl enable filmpin.service',
+      'sudo systemctl restart filmpin.service',
+      'sudo systemctl enable filmpinclient.service',
+      'sudo systemctl restart filmpinclient.service',
+      'sudo systemctl restart caddy'
+    ];
     runCommandsInShell(commands);
-  } else {
-    console.log('No relevant changes detected, no services restarted.');
+  } catch (error) {
+    console.error('Failed to update and restart services:', error);
+    throw error;
   }
 };
 
 app.get('/', (_, res) => res.sendStatus(200));
 
-app.post('/webhook', verifyGitHubSignature, (req, res) => {
+app.post('/webhook', verifyGitHubSignature, async (req, res) => {
   try {
-    const payload = JSON.parse(req.body);
-    const commits = payload.commits || [];
-    updateAndRestartServices(commits);
+    updateAndRestartServices();
     res.status(200).send('Success');
   } catch (error) {
-    console.error('Failed to process webhook:', error);
-    res.status(500).send('Error updating services');
-  }
-});
-
-app.get('/webhook', verifyGitHubSignature, (req, res) => {
-  try {
-    const payload = JSON.parse(req.body);
-    const commits = payload.commits || [];
-    updateAndRestartServices(commits);
-    res.status(200).send('Success');
-  } catch (error) {
-    console.error('Failed to process webhook:', error);
     res.status(500).send('Error updating services');
   }
 });
