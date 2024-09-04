2c9316ac93d0c95eac55f1e1f07199ea394ba3fc
Justine Paralejas <paralejasjustine15@gmail.com>
2024-09-04 20:33:21 +0800

    margin fix

--- client/src/components/sections/EpisodeSection.jsx
+++ client/src/components/sections/EpisodeSection.jsx
@@ -4,7 +4,7 @@ import Episode from '../Episode';
 
 const EpisodeSection = ({ episodes, showId, title, backdropPath, seasonNumber }) => {
 	return (
-		<section className='w-full ml-4 mr-4 mb-4 bg-transparent overflow-hidden gap-4'>
+		<section className='w-full bg-transparent overflow-hidden gap-4'>
 			<h1 className='text-primary-foreground pb-4 text-sm font-semibold'>Episodes</h1>
 			<Swiper {...swiperConfig}>
 				{episodes && episodes.map((episode, index) => (
