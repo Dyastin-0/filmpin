d3b642931374ca6e27444b92717f62c6b8164231
Justine <paralejasjustine15@gmail.com>
2024-09-25 02:24:48 +0800

    Added a new origin.

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "https://filmpin.dyastin.me"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://filmpin.dyastin.me",
+  "https://filmpin.dyastin.tech",
+];
 
 module.exports = allowedOrigins;
