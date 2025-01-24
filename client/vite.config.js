5641f6b1b4ac8d54a0989f6f5eb499d4b5a59c05
Justine <paralejasjustine15@gmail.com>
2025-01-24 20:11:50 +0800

    Update package, fix chunks

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react";
+            if (id.includes("lodash")) return "lodash";
+            if (id.includes("fortawesome")) return "fortawesome";
+            if (id.includes("dnd-kit")) return "dnd-kit";
+            if (id.includes("emotion")) return "emotion";
+            if (id.includes("autoprefixer")) return "autoprefixer";
+            if (id.includes("swiper")) return "swiper";
+            if (id.includes("socket.io-client")) return "socket.io-client";
+            if (id.includes("framer-motion")) return "framer-motion";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
